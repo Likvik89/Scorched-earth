4116ff207bffbf18ce1bdfd7f795274c4d0337d2
Likvik <erik@elfisk.dk>
2025-01-08 11:07:59 +0100

    more js

--- js/script.js
+++ js/script.js
@@ -2,4 +2,10 @@ window.onload=function() {
     var canvas = document.getElementById("battlefield");
     var context = canvas.getContext("2d");
     var redtank = new Image();
+    redtank.src = "sprites/red_tank.png";
+
+    context.clearRect(0,0,1024,800);
+    context.drawImage(redtank,300,300);
+
+
 }
