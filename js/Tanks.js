0cf1b70bde39a0a5f86abc8e47647038b5f2f5c2
Mads-Emilbl <madsemilbl@gmail.com>
2025-01-16 14:19:31 +0100

    Lavede start movement i Tank.js

--- js/Tanks.js
+++ js/Tanks.js
@@ -1,3 +1,50 @@
-export function shoot () {
+/**  SKAL KALDE EN ANDEN FUNKTION! IKKE SLET 
+ * export function shoot () {
     
 }
+**/
+window.onload=function movement(){
+    //Id forkert?
+    var canvas = document.getElementById("battlefield");
+    var context = canvas.getContext("2d");
+    var tank1 = new Image();
+    tank1.src = "./sprites/blue_tank.png";
+    xpos = 400;
+    ypos = 512;
+
+
+    //Tegner tank1 fra context
+    function draw(context){
+        context.drawImage(tank1, xpos,ypos);
+        context.restore();
+    }
+
+    function animate(){
+        
+    }
+
+    // eventlistnere
+	document.addEventListener("keydown", keyDownHandler, false);
+	document.addEventListener("keyup", keyUpHandler, false);
+	// funktioner til tastatur
+	function keyDownHandler(e) {
+    if(e.keyCode == 38) {
+        upPressed = true;
+		}
+    else if(e.keyCode == 40) {
+        downPressed = true;
+		}
+	else if(e.keyCode == 32){
+		skud = true;
+		pposY = ypos + 5;
+		}
+	}
+	function keyUpHandler(e) {
+    if(e.keyCode == 38) {
+        upPressed = false;
+		}
+    else if(e.keyCode == 40) {
+        downPressed = false;
+		}
+	}
+};
