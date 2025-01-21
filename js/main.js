2f39fc070c8ea16946929242d7f9fb2c6c0c7f0a
Likvik <erik@elfisk.dk>
2025-01-21 10:50:35 +0100

    image loading setup

--- js/main.js
+++ js/main.js
@@ -5,16 +5,33 @@ window.onload=function() {
     var canvas = document.getElementById("battlefield");
     var context = canvas.getContext("2d");
 
+    var REDTANK = new Image();
+    REDTANK.src = "sprites/red_tank.png"
+
     var BLUETANK = new Image();
     BLUETANK.src = "sprites/blue_tank.png";
 
+    var loaded_images = 0;
+    var images = 2;
+
+    function load_image() {
+        loaded_images++;
+        if (loaded_images >= images) {
+            //                   img        x          y
+            context.drawImage(BLUETANK, BLUETANKx, BLUETANKy);
+            console.log(context);
+        }
+    }
+
     //draw tank after it has loaded
     BLUETANK.onload = function(){
-        //                 img      x           y
-        context.drawImage(BLUETANK, BLUETANKx, BLUETANKy);
+        load_image();
     };
+
+    REDTANK.onload = function(){
+        load_image();
+    }
     
-    console.log(context);
-    
+
 
 }
