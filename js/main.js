918400e6a57cdf05bf48c3f869574031451024de
Likvik <erik@elfisk.dk>
2025-01-22 07:51:58 +0100

    some cleanup + trying to make animate function correctly (it doesn't (yet))

--- js/main.js
+++ js/main.js
@@ -3,44 +3,46 @@ window.onload=function() {
     
     movement();    
 
-
-
     var loaded_images = 0;
     var images = 2;
 
     function load_image() {
         loaded_images++;
-        if (loaded_images >= images) {
-            animate();
 
+        if (loaded_images == images) {
+            animate();
+            console.log(animate);
         }
     }
 
     //draw tank after it has loaded
     BLUETANK.onload = function(){
         load_image();
+        console.log("bluetank loaded");
     };
 
     REDTANK.onload = function(){
         load_image();
+        console.log("redtank loaded");
     }
 }
 
-function start(){
-
-    //game_loop()
+function draw(context){
+    //                   img        x          y
+    context.drawImage(BLUETANK, BLUETANKx, BLUETANKy);
+    context.drawImage(REDTANK, REDTANKx, REDTANKy);
+    context.restore();
 }
 
 function animate(){
     var canvas = document.getElementById("battlefield");
     var context = canvas.getContext("2d");
-    //                   img        x          y
-    context.drawImage(BLUETANK, BLUETANKx, BLUETANKy);
+
+    context.clearRect(0, 0, 1000, 800);
+    draw(context);
+    setTimeout(animate,5);
+
+
     console.log(context);
 
 }
-/*
-function game_loop() {
-
-}
-*/
